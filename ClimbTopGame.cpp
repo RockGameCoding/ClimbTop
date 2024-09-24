1c069ae439c5500b02b5430670a8b9d9d684b406
RockGameCoding <krh0323@pukyong.ac.kr>
2024-09-24 15:44:14 +0900

    ClimbTop Code

---
+++ ClimbTopGame.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClimbTopGame.h"
+#include "Blueprint/UserWidget.h"
+#include "TimerManager.h"
+#include "ClimbTopPlayerController.h"
+#include "Kismet/GameplayStatics.h"
+
+AClimbTopGame::AClimbTopGame()
+{
+    PlayerControllerClass = AClimbTopPlayerController::StaticClass();
+}
+
+void AClimbTopGame::BeginPlay()
+{
+    Super::BeginPlay();
+    UUserWidget* HUD = CreateWidget(GetWorld(), MyHUDClass);
+    if(HUD != nullptr)
+    {
+        HUD->AddToViewport();
+    }
+    HandleGameStart();
+}
+
+void AClimbTopGame::GameEnd()
+{
+    UUserWidget* GameEndWidget = CreateWidget(GetWorld(), GameEndClass);
+    if(GameEndWidget != nullptr)
+    {
+        GameEndWidget->AddToViewport();
+    }
+}
+
+void AClimbTopGame::HandleGameStart()
+{
+    PlayerController = Cast<AClimbTopPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+    if(PlayerController)
+    {
+        PlayerController->SetPlayerEnabledState(false);
+        //Set Timer(StartDelay)
+        FTimerHandle PlayerEnableTimerHandle;
+        FTimerDelegate PlayerEnableTimerDelagate = FTimerDelegate::CreateUObject(
+            PlayerController, 
+            &AClimbTopPlayerController::SetPlayerEnabledState, 
+            true);
+        GetWorldTimerManager().SetTimer(PlayerEnableTimerHandle, PlayerEnableTimerDelagate, StartDelay, false);
+    }
+}
---
+++ ClimbTopGame.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClimbTopGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIMBTOP_API AClimbTopGame : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AClimbTopGame();
+
+	void GameEnd();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> MyHUDClass;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> GameEndClass;
+
+	void HandleGameStart();
+
+	class AClimbTopPlayerController* PlayerController;
+
+	float StartDelay = 4.f;
+};
